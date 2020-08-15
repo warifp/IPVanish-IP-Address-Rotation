0a4d94769ea955cc752d25f54fefc4f923a09ec7
Wahyu Arif Purnomo <warifp@gmail.com>
2020-08-15 07:11:46 +0700

    add configuration

---
+++ config/configuration.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ *  Confirguration IP Vanish Premium
+ *  Support SOCKS5
+ */
+
+$username = 'USERNAME-IPVANISH';
+$password = 'PASSWORD-IPVANISH';
+$servers = $IPVanish->servers();
+$port = '1080';
